4c4eca12292a3b5d07d61456d72e93f603f7d193
scvrylullaby <vadimzooru@gmail.com>
2026-05-30 22:46:54 +0800

    feat: add dockerfiles, rmq

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core'
 
 import { AppModule } from './core/app.module'
 import { createGrpcServer } from './infrastructure/grpc/grpc.server'
+import { createRabbitMQServer } from './infrastructure/rabbitmq/rabbitmq.server'
 import './observability/tracing'
 
 async function bootstrap() {
@@ -10,6 +11,7 @@ async function bootstrap() {
 	const config = app.get(ConfigService)
 
 	createGrpcServer(app, config)
+	createRabbitMQServer(app, config)
 
 	await app.startAllMicroservices()
 
